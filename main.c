d51db06fac104823049b5f5aaff2f6ea6c4d45ab
manianu <51318832+manianu@users.noreply.github.com>
2019-06-14 11:40:55 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 /*
- * 7segment.c
+ * motordriver.c
  *
- * Created: 10-06-2019 10:05:25
+ * Created: 11-06-2019 10:10:07
  * Author : ganga
  */ 
-#ifndef F_CPU
+#ifdef F_CPU
 #define F_CPU 16000000UL
 #endif
 #include <avr/io.h>
@@ -12,42 +12,13 @@
 
 int main(void)
 {
-    DDRD=0XFF;
+	DDRD=0X00;
     while (1) 
     {
-		PORTD=0X3F;
+		PORTB=0XFF;
 		_delay_ms(2000);
-		PORTD=0X06;
+		PORTB=0X00;
 		_delay_ms(2000);
-		PORTD=0X5B;
-		_delay_ms(2000);
-		PORTD=0X4F;
-		_delay_ms(2000);
-		PORTD=0X66;
-		_delay_ms(2000);
-		PORTD=0X6D;
-		_delay_ms(2000);
-		PORTD=0X7D;
-		_delay_ms(2000);
-		PORTD=0X07;
-		_delay_ms(2000);
-		PORTD=0XFF;
-		_delay_ms(2000);
-		PORTD=0X67;
-		_delay_ms(2000);
-		PORTD=0X77;
-		_delay_ms(2000);
-		PORTD=0X7C;
-		_delay_ms(2000);
-		PORTD=0X39;
-		_delay_ms(2000);
-		PORTD=0X5E;
-		_delay_ms(2000);
-		PORTD=0X79;
-		_delay_ms(2000);
-		PORTD=0X71;
-		_delay_ms(2000);
-		
-			}
+    }
 }
 

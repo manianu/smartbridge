7076c1651ac0096e64e9126dd8763d08e760b094
manianu <51318832+manianu@users.noreply.github.com>
2019-06-14 11:43:10 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,36 +1,92 @@
 /*
- * pwmmotor.c
+ * potentiometer.c
  *
- * Created: 11-06-2019 11:32:00
+ * Created: 10-06-2019 14:42:40
  * Author : ganga
  */ 
 
-#define F_CPU 8000000UL
 #include <avr/io.h>
-# include <util/delay.h>
-void PWM_init()
-{
-	TCCR0=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS00);
-	DDRB=(1<<PB3);
-}
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <stdlib.h>
 
-int main()
+#define E 5
+#define RS 7
+
+void send_a_command(unsigned char command);
+void send_a_character(unsigned char character);
+void send_a_string(char *string_of_characters);
+int main(void)
 {
-	unsigned char duty;
-	PWM_init();
-	while (1)
+
+	DDRC=0XFF;
+	DDRA=0X00;
+	DDRD=0XFF;
+	DDRB=0XFF;
+	
+	
+	_delay_ms(50);
+	
+	ADMUX|=(1<<REFS0)|(1<<REFS1);
+	ADCSRA|=(1<<ADEN)|(1<<ADATE)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
+	int  COUNTA =0;
+	char SHOWA[3];
+	send_a_command(0X01);
+	_delay_ms(50);
+	send_a_command(0X38);
+	_delay_ms(50);
+	send_a_command(0x0E);
+	send_a_command(0X80+0);
+	while(COUNTA>0)
 	{
-		for(duty=0;duty<255;duty++)
+		PORTB=COUNTA;
+	
+	PORTB=0X00;
+
+	_delay_ms(50);
+}
+ 
+while(1)
+{
+	COUNTA =ADC/4;
+	 {
+		send_a_string("Smartbridge");
+		send_a_command(0X80+0X40+0);
+		send_a_string("Temp(c)=");
+		send_a_command(0X80+0X40+8);
+		itoa(COUNTA,SHOWA,10);
+		send_a_string(SHOWA);
+		send_a_string("");
+	}
+	void send_a_string(char*string_of_characters)
+	{
+		while(*string_of_characters > 0)
 		{
-			OCR0=duty;
-			_delay_ms(8);
-		}
-		for(duty=255;duty>1;duty--)
-		{
-			OCR0=duty;
-			_delay_ms(8);
+			send_a_character(*string_of_characters++);
 		}
 	}
-}
+	void send_a_command(unsigned char command)
+	{
+		PORTC=command;
+		PORTD&=~(1<<RS);
+		PORTD|=(1<<E);
+		_delay_ms(50);
+		PORTD&=~(1<<E);
+		PORTC=0;
+	}
+	void send_a_character(unsigned char character)
+	{
+		PORTC=character;
+		
+		PORTD|=(1<<RS);
+		PORTD|=(1<<E);
+		
+		_delay_ms(50);
+		PORTD&=~(1<<E);
+		PORTC=0;
+		
+	}
+ }
+
 
 
